5bacee7f21e3932dd664ffb39f72091136fbc11e
Ibrahim Saber <ibrahimsaber622@gmail.com>
2025-11-26 01:34:17 +0200

    add: basic menu data

---
+++ src/data/translations.js
@@ -0,0 +1,24 @@
+export const translations = {
+  ar: {
+    home: "الرئيسية",
+    menu: "قائمة الطعام",
+    contact: "تواصل معنا",
+    about: "من نحن",
+    currency: "ريال",
+    filterAll: "الكل",
+    filterChicken: "الدجاج والأرز",
+    filterGrill: "المشويات",
+    filterMeat: "اللحوم"
+  },
+  en: {
+    home: "Home",
+    menu: "Menu",
+    contact: "Contact",
+    about: "About Us",
+    currency: "SAR",
+    filterAll: "All",
+    filterChicken: "Chicken & Rice",
+    filterGrill: "Grills",
+    filterMeat: "Meat"
+  }
+};
